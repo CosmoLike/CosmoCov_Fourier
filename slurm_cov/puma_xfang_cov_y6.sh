3660c254b68097c1770cca8a052a7dc3de7b9753
xfangcosmo <fang.307@osu.edu>
2022-02-03 15:33:03 -0800

    reset job walltime limit

--- slurm_cov/puma_xfang_cov_y6.sh
+++ slurm_cov/puma_xfang_cov_y6.sh
@@ -3,20 +3,20 @@
 #SBATCH --account=timeifler
 #SBATCH --partition=high_priority
 #SBATCH --qos=user_qos_timeifler
-#SBATCH --nodes=11
-#SBATCH --ntasks=1000
-#SBATCH --array=0-11
+#SBATCH --nodes=1
+#SBATCH --ntasks=94
+#SBATCH --array=0-125
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1gb
-#SBATCH --time=100:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output=%A.out
 #SBATCH --error=%A.err
 cd $SLURM_SUBMIT_DIR
 module load gsl/2.6
 module load python/3.6/3.6.5
 N=11781
-for i in {0..999}; do
-	i_cov=$((SLURM_ARRAY_TASK_ID*1000+i+1));
+for i in {0..93}; do
+	i_cov=$((SLURM_ARRAY_TASK_ID*94+i+1));
 	if [[ ${i_cov} -gt N ]]; then break; fi;
 	srun --ntasks 1 --exclusive -c 1 ./compute_covariances_fourier_10x2pt ${i_cov} 1 >&/home/u1/xfang/output/job_output_${i_cov}.log
 done;
