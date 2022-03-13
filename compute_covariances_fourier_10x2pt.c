0b638921ff9738b9adcf9c7d3e0e574aff5d99ea
xfangcosmo <fang.307@osu.edu>
2022-03-13 09:10:20 -0700

    new scales

--- compute_covariances_fourier_10x2pt.c
+++ compute_covariances_fourier_10x2pt.c
@@ -182,7 +182,8 @@ int main(int argc, char** argv)
   // init_cosmo_runmode("halofit");
   init_cosmo_runmode("halomodel");
   // init_binning_fourier(20,30.0,3000.0,3000.0,21.0,10,10);
-  init_binning_fourier(15,20.0,3000.0,3000.0,0.0,10,10);
+  // init_binning_fourier(15,20.0,3000.0,3000.0,0.0,10,10);
+  init_binning_fourier(25,20.0,7979.0,7979.0,0.0,10,10);
   init_survey(survey_designation[t],nsource_table[t],nlens_table[t],area_table[t]);
   sprintf(arg1,"zdistris/%s",source_zfile[t]);
   sprintf(arg2,"zdistris/%s",lens_zfile[t]); 
