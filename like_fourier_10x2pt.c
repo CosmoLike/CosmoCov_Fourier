ace4262191415ac9b86ec1e2259b36bd7ffb3583
xfangcosmo <fang.307@osu.edu>
2022-03-26 15:43:57 -0400

    add new datav under updated nzs

--- like_fourier_10x2pt.c
+++ like_fourier_10x2pt.c
@@ -963,9 +963,11 @@ void save_zdistr_lenses(int zl){
   double sigma_zphot_clustering[3]={0.03,0.03};
 
   double area_table[2]={12300.0,16500.0}; // Y1 corresponds to DESC SRD Y1, Y6 corresponds to assuming that we cover the full SO area=0.4*fsky and at a depth of 26.1 which is in a range of reasonable scenarios (see https://github.com/LSSTDESC/ObsStrat/tree/static/static )
-  double nsource_table[2]={11.0,23.0};
-  double nlens_table[2]={18.0,41.0};
+  // double nsource_table[2]={11.0,23.0};
+  // double nlens_table[2]={18.0,41.0};
 
+  double nsource_table[2]={10.7,22.5};
+  double nlens_table[2]={13.1,26.8};
 
   char survey_designation[2][200]={"LSSTxSO_Y1","LSSTxSO_Y6"};
   char tomo_binning_source[2][200]={"source_std","source_std"};
