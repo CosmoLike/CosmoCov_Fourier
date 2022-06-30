b4166d07c6db8420f3f977b38d14e800a2e71787
xfangcosmo <fang.307@osu.edu>
2022-06-29 22:13:07 -0700

    add 3x2pthf script

---
+++ slurm_cov/puma_xfang_cov_y6_3x2pt_fft_hf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=LSSTxSO_cov
+#SBATCH --account=timeifler
+#SBATCH --partition=high_priority
+#SBATCH --qos=user_qos_timeifler
+#SBATCH --nodes=1
+#SBATCH --ntasks=15
+#SBATCH --array=0-460
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1gb
+#SBATCH --time=15:00:00
+#SBATCH --output=%A.out
+#SBATCH --error=%A.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=xfang@email.arizona.edu
+
+cd $SLURM_SUBMIT_DIR
+module load gsl/2.6
+module load python/3.6/3.6.5
+N=6903
+for ((i = 0; i < SLURM_NTASKS; i++)); do
+	i_cov=$((SLURM_ARRAY_TASK_ID*SLURM_NTASKS+i+1));
+	if [[ ${i_cov} -gt N ]]; then break; fi;
+	srun --ntasks 1 --exclusive -c 1 ./compute_covariances_fourier_3x2pt_fft_hf ${i_cov} 1 >&/home/u1/xfang/output/job_covy6_${i_cov}.log
+done;
