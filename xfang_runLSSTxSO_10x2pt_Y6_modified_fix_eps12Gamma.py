e174002eb12b60d798884a593c8c8153f8cdcfc3
xfangcosmo <fang.307@osu.edu>
2022-10-26 22:37:32 -0400

    fix e1=0.1 fid

--- xfang_runLSSTxSO_10x2pt_Y6_modified_fix_eps12Gamma.py
+++ xfang_runLSSTxSO_10x2pt_Y6_modified_fix_eps12Gamma.py
@@ -71,5 +71,5 @@ sample_params = sample_cosmology_10x2_fix_eps12Gamma(get_N_tomo_shear(),get_N_to
 
 Nwalker = int(sys.argv[1])
 # sample_main(sample_params,sigma_z_shear[model],sigma_z_clustering[model],8000,Nwalker,1,chain_file, blind=False, pool=MPIPool())
-sample_main_e1(sample_params,sigma_z_shear[model],sigma_z_clustering[model],8000,Nwalker,1,chain_file, blind=False, pool=MPIPool())
+sample_main(sample_params,sigma_z_shear[model],sigma_z_clustering[model],8000,Nwalker,1,chain_file, blind=False, pool=MPIPool())
 
--- xfang_runLSSTxSO_10x2pt_Y6_modified_fixhalo-e1.py
+++ xfang_runLSSTxSO_10x2pt_Y6_modified_fixhalo-e1.py
@@ -70,5 +70,5 @@ skip_shearcalib_phz_sampling()
 sample_params = sample_cosmology_10x2_fixhalo(get_N_tomo_shear(),get_N_tomo_clustering(), MG=False, w0wa=False, cov_modified=True)
 
 Nwalker = int(sys.argv[1])
-sample_main_e1(sample_params,sigma_z_shear[model],sigma_z_clustering[model],8000,Nwalker,1,chain_file, blind=False, pool=MPIPool())
+sample_main(sample_params,sigma_z_shear[model],sigma_z_clustering[model],8000,Nwalker,1,chain_file, blind=False, pool=MPIPool())
 
